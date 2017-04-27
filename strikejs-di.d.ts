50d895f8ec6a35f4cbd8a27ec5ba89f6d439c4ce
Suhail Abood <suhail.abood@gmail.com>
2017-04-27 20:33:23 +0100

    Fixes exports in typings.

--- strikejs-di.d.ts
+++ strikejs-di.d.ts
@@ -1,20 +1,20 @@
 declare module "strikejs-di" {
-    export type Iterator<T> = (o:T,i:number|string)=>void; 
+    type Iterator<T> = (o:T,i:number|string)=>void; 
 
-    export type Dictionary<T> = {
+    type Dictionary<T> = {
         [idx:string]:T;
     }
 
-    export function forEach<T>(obj:Dictionary<T>|T[],iterator:Iterator<T>):void;
+    function forEach<T>(obj:Dictionary<T>|T[],iterator:Iterator<T>):void;
 
-    export interface DependencyStore {
+    interface DependencyStore {
         get<T>(key:string):T; 
     }
 
-    export interface DependencyInjectorProps {
+    interface DependencyInjectorProps {
         injector:DependencyStore; 
         children?:any;
     }
 
-    export function DI(props:DependencyInjectorProps):React.ReactElement<any>;
+    export = function DI(props:DependencyInjectorProps):React.ReactElement<any>;
 }
